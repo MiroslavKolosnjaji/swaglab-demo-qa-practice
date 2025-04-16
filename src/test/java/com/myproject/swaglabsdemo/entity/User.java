6b3b143d4d39eeb1b7fdb7aea567eb21d61a3946
Miroslav <kolosnjaji@stcable.net>
2025-04-16 19:20:26 +0200

    feat(entity): add User class

--- src/test/java/com/myproject/swaglabsdemo/entity/User.java
+++ src/test/java/com/myproject/swaglabsdemo/entity/User.java
@@ -1,7 +1,21 @@
 package com.myproject.swaglabsdemo.entity;
 
+import lombok.*;
+
 /**
-@author Miroslav Kološnjaji
-*/
-    public class User {
+ * @author Miroslav Kološnjaji
+ */
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    private String userName;
+    private String password;
+    private String postalCode;
+    private String firstName;
+    private String lastName;
+
 }
