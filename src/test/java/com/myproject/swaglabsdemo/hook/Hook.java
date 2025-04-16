f09098f0b16296e9e9d03ed9ddb16ce684ac7cba
Miroslav <kolosnjaji@stcable.net>
2025-04-16 19:21:35 +0200

    feat(hook): add Hook class

--- src/test/java/com/myproject/swaglabsdemo/hook/Hook.java
+++ src/test/java/com/myproject/swaglabsdemo/hook/Hook.java
@@ -1,29 +1,57 @@
 package com.myproject.swaglabsdemo.hook;
 
-import com.myproject.swaglabsdemo.driver.DriverManager;
+import com.google.inject.Inject;
+import com.myproject.swaglabsdemo.util.config.NavigationService;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
+import lombok.extern.slf4j.Slf4j;
 import org.openqa.selenium.WebDriver;
 
 /**
  * @author Miroslav Kološnjaji
  */
+@Slf4j
 public class Hook {
 
-    private WebDriver webDriver;
+    private final WebDriver webDriver;
+
+    @Inject
+    public Hook(WebDriver webDriver) {
+        this.webDriver = webDriver;
+    }
 
     @Before
     public void setUp() {
-        DriverManager driverManager = DriverManager.getInstance();
-        driverManager.setDriver(DriverManager.Driver.CHROME);
-
-        webDriver = driverManager.getWebDriver();
-        webDriver.manage().window().maximize();
+        openLoginPage();
     }
 
     @After
-    public void tearDown(){
-        if(webDriver != null)
+    public void tearDown() {
+        if (webDriver != null)
             webDriver.quit();
     }
+
+    private void openLoginPage() {
+
+        for (int i = 0; i < 5; i++)
+            if (isOpen())
+                break;
+
+    }
+
+
+    public boolean isOpen() {
+
+        boolean isOpen = false;
+
+        try {
+            NavigationService.openLoginPage(webDriver);
+            isOpen = true;
+        } catch (Exception e) {
+            log.error(e.getMessage());
+        }
+
+        return isOpen && webDriver.getTitle().equals("Swag Labs");
+    }
+
 }
