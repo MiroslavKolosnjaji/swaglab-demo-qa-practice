0d9fb136d7f0e46a248182e1b7259aaf85cc1880
Miroslav <kolosnjaji@stcable.net>
2025-04-16 19:15:41 +0200

    feat(page): add YourCartPage class

--- src/test/java/com/myproject/swaglabsdemo/page/YourCartPage.java
+++ src/test/java/com/myproject/swaglabsdemo/page/YourCartPage.java
@@ -1,7 +1,34 @@
 package com.myproject.swaglabsdemo.page;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.CacheLookup;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 /**
-@author Miroslav Kološnjaji
-*/
-    public class YourCartPage {
+ * @author Miroslav Kološnjaji
+ */
+public class YourCartPage extends BasePage {
+
+    private final String TITLE = "Your Cart";
+
+    @FindBy(xpath = "//button[@id = 'checkout']")
+    @CacheLookup
+    private WebElement btnCheckout;
+
+    protected YourCartPage(WebDriver webDriver) {
+        super(webDriver);
+    }
+
+    public CheckoutYourInformationPage clickCheckout() {
+        btnCheckout.click();
+        return PageFactory.initElements(webDriver, CheckoutYourInformationPage.class);
+    }
+
+    @Override
+    public boolean isCorrectPage() {
+        return webDriver.findElement(By.xpath(XPATH_TO_PAGE_TITLE)).getText().equals(TITLE);
+    }
 }
