15f460f902d76ee4fad37ecb216b13d3648d4c4e
Miroslav <kolosnjaji@stcable.net>
2025-04-16 19:21:08 +0200

    feat(util): add NavigationService class

--- src/test/java/com/myproject/swaglabsdemo/util/config/NavigationService.java
+++ src/test/java/com/myproject/swaglabsdemo/util/config/NavigationService.java
@@ -1,7 +1,27 @@
 package com.myproject.swaglabsdemo.util.config;
 
+import com.myproject.swaglabsdemo.page.LoginPage;
+import com.myproject.swaglabsdemo.page.YourCartPage;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+
 /**
-@author Miroslav Kološnjaji
-*/
-    public class NavigationService {
+ * @author Miroslav Kološnjaji
+ */
+public class NavigationService {
+
+    private static final String XPATH_TO_SHOPPING_CART_LINK = "//div[@id='shoping_cart_container']//a";
+    private static final String URL = "https://www.saucedemo.com/";
+
+
+    public static LoginPage openLoginPage(WebDriver webDriver){
+        webDriver.get(URL);
+        return PageFactory.initElements(webDriver, LoginPage.class);
+    }
+
+    public YourCartPage navigateToShoppingCartPage(WebDriver webDriver){
+        webDriver.findElement(By.xpath(XPATH_TO_SHOPPING_CART_LINK)).click();
+        return PageFactory.initElements(webDriver, YourCartPage.class);
+    }
 }
