c60e9a389e741ce0afd49cf596b7bf8beb5d577d
Miroslav <kolosnjaji@stcable.net>
2025-04-18 21:48:19 +0200

    fix(runner): update feature path.

--- src/test/java/com/myproject/swaglabsdemo/runner/TestRunner.java
+++ src/test/java/com/myproject/swaglabsdemo/runner/TestRunner.java
@@ -2,12 +2,13 @@ package com.myproject.swaglabsdemo.runner;
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.DataProvider;
 
 /**
  * @author Miroslav Kološnjaji
  */
 @CucumberOptions(
-        features = "src/test/resources/features",
+        features = "src/test/resources/features/",
         glue = {"com/myproject/swaglabsdemo/stepDefinition", "com/myproject/swaglabsdemo/hook"},
         plugin = {"pretty", "html:target/cucumber-reports.html"}
 )
