3746c6c6c719089f9bcf69da84f0e88b6be8be5f
Miroslav <kolosnjaji@stcable.net>
2025-04-18 21:28:45 +0200

    refactor(config): initialize WebDriverUtils within WebDriver provider.

--- src/test/java/com/myproject/swaglabsdemo/util/config/E2EModule.java
+++ src/test/java/com/myproject/swaglabsdemo/util/config/E2EModule.java
@@ -3,19 +3,41 @@ package com.myproject.swaglabsdemo.util.config;
 import com.google.inject.AbstractModule;
 import com.google.inject.Inject;
 import com.google.inject.Provides;
+import com.myproject.swaglabsdemo.entity.strategy.TestDataStrategy;
+import com.myproject.swaglabsdemo.entity.strategy.impl.ValidUserStrategy;
+import com.myproject.swaglabsdemo.stepDefinition.OrderingStep;
+import com.myproject.swaglabsdemo.util.WebDriverUtils;
 import com.myproject.swaglabsdemo.util.driver.DriverProviderService;
 import io.cucumber.guice.ScenarioScoped;
 import org.openqa.selenium.WebDriver;
+
+import java.time.Duration;
 
 /**
  * @author Miroslav Kološnjaji
  */
 public class E2EModule extends AbstractModule {
 
+    @Override
+    protected void configure() {
+        bind(OrderingStep.class).in(ScenarioScoped.class);
+        super.configure();
+    }
+
     @Provides
     @ScenarioScoped
     @Inject
     public synchronized WebDriver provideWebDriver(){
-        return DriverProviderService.getChromeDriver();
+
+        WebDriver webDriver = DriverProviderService.getChromeDriver();
+        WebDriverUtils.createInstance(webDriver, Duration.ofSeconds(10));
+
+        return webDriver;
+    }
+
+    @Provides
+    @ScenarioScoped
+    @Inject
+    public synchronized TestDataStrategy provideUserData(){ return new ValidUserStrategy();
     }
 }
