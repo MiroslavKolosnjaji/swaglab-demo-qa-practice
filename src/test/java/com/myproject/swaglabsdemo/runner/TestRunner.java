e8083ccd942e04593b2c68156e2d22fdc6a4ac59
Miroslav <kolosnjaji@stcable.net>
2025-04-22 21:16:53 +0200

    chore(runner): remove unnecessary imports.

--- src/test/java/com/myproject/swaglabsdemo/runner/TestRunner.java
+++ src/test/java/com/myproject/swaglabsdemo/runner/TestRunner.java
@@ -2,7 +2,6 @@ package com.myproject.swaglabsdemo.runner;
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
-import org.testng.annotations.DataProvider;
 
 /**
  * @author Miroslav Kološnjaji
